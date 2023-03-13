dc52aabe4407b3c447263f04fcda3dccefd08b2e
TEAM3 <david@nethunters.xyz>
2023-03-13 05:04:39 +0000

    Adding contract conector and display contract total supply

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { Wallet, ethers } from 'ethers';
+import { Wallet, ethers, Contract, BigNumber } from 'ethers';
 import { env } from '../environment/env';
+import { HttpClient } from '@angular/common/http'; 
+import tokenJson from '../assets/MyToken.json';
+
+const API_URL = 'http://10.162.235.88:3000/contract-address';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +17,13 @@ export class AppComponent {
   userWallet: Wallet | undefined;
   userEthBalance: number | undefined;
   importedWallet: boolean;
+  userTokenBalance: number | undefined;
+  tokenContractAddress: string | undefined;
+  tokenContract: Contract | undefined;
+  tokenTotalSupply: number | string | undefined;
 
 
-  constructor() {
+  constructor(private http: HttpClient) {
     //this.provider = ethers.getDefaultProvider('goerli');
     this.provider = new ethers.providers.InfuraProvider(
     "maticmum",
@@ -24,6 +32,11 @@ export class AppComponent {
 
     // create logic to verify if is or not an imported 
     this.importedWallet = false;
+    //this.tokenContractAddress =
+  }
+
+  getTokenAddress(){
+    return this.http.get<{ address: string }>(API_URL);
   }
 
   syncBlock(){
@@ -31,8 +44,25 @@ export class AppComponent {
     this.provider.getBlock('latest').then((block) => {
       this.blockNumber = block.number;
     })
+    this.getTokenAddress().subscribe((response) => {
+      this.tokenContractAddress = response.address;
+      this.updateTokenInfo();
+    })  
   }
 
+  updateTokenInfo() {
+    if (!this.tokenContractAddress) return;
+    this.tokenContract = new Contract(
+      this.tokenContractAddress,
+      tokenJson.abi,
+      this.userWallet ?? this.provider
+    );
+    this.tokenTotalSupply = 'loading...';
+    this.tokenContract['totalSupply']().then((totalSupplyBN: BigNumber) => {
+      const totalSupplyStr = ethers.utils.formatEther(totalSupplyBN);
+      this.tokenTotalSupply = parseFloat(totalSupplyStr);
+    });
+  } 
   clearBlock() {
     this.blockNumber = 0;
   }
